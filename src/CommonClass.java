5caaecabe17b6c2718b9b0eadeb8dd519292e595
darkwater86 <caikaiqiang10086@outlook.com>
2017-07-08 19:23:57 +0800

    We startes with the easist character of Java.

--- src/CommonClass.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by lenovo1 on 2017/7/8.
- */
-//功能性演示
-public class CommonClass {
-    public static void main(String[] args){
-        
-    }
-}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Mr.Darkwater on 2017/7/8.
+ */
+public class Main {
+
+}
